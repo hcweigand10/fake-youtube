bc761948e607ff4696e3cba5415a9ae26204d687
hcweigand10 <henryweigand10@gmail.com>
2022-06-02 16:58:47 -0700

    warnings

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./Results.css"
 import VideoItem from "../videoItem/VideoItem"
 
 const Results = (props) => {
     const temp = props.results
     const raw = props.results.filter((video)=>{
-        return temp.indexOf(video) != props.featureIndex
+        return temp.indexOf(video) !== props.featureIndex
     })
     const videos = raw.map((video)=> {
         return <VideoItem key={video.id.videoId} index={props.results.indexOf(video)} video={video.snippet} onSelect={props.onSelect}/>
--- src/components/videoItem/VideoItem.js
+++ src/components/videoItem/VideoItem.js
@@ -8,7 +8,7 @@ const VideoItem = (props) => {
     return (
         <div key={props.key} onClick={onSelect} className="ui grid segment">
             <div  className="five wide column">
-                <img className="thumbnail" src={props.video.thumbnails.default.url}/>
+                <img className="thumbnail" src={props.video.thumbnails.default.url} alt={props.video.title}/>
             </div>
             <div className="eleven wide column">
                 <h3 className="title">{props.video.title}</h3>
